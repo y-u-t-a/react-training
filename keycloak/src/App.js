eedf06cc70135e6ae6e92bab0fa92b43583797e6
Yuta Ueno <y.ueno0803@gmail.com>
2020-05-03 01:47:01 +0900

    react-bootstrap を導入

--- keycloak/src/App.js
+++ keycloak/src/App.js
@@ -3,6 +3,10 @@ import {BrowserRouter, Route} from 'react-router-dom'
 import Top from './top/Top'
 import Users from './users/Users'
 
+// react-bootstrap を使うために import する
+// App で import することで子コンポーネントでも有効になる
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 export default class App extends React.Component {
   render() {
     return (
